a6d7cc1c2eba9e1a608e654447977e73d3a8981e
Saleh Ibne Omar <35599978+SalehIbneOmar@users.noreply.github.com>
2019-01-29 13:10:35 +0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function preLoader(){
-    setTimeout(function(){
-       document.getElementById("loading_screen").style.display = "none";
-    }, 5000);
-}
-window.addEventListener("load", preLoader); 
